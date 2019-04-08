cd6bddd40bd1958bc6d156f1dc4df02d931203a8
Víctor J. Sánchez <vjsanchezr@hotmail.com>
2019-04-08 11:39:55 -0500

    Update ex03.js

--- public/javascripts/views/ex03.js
+++ public/javascripts/views/ex03.js
@@ -1,32 +1,60 @@
-const model = tf.sequential();
+$(() =>{
 
-model.add(tf.layers.dense({ units: 4, inputShape: [2], activation: 'tanh' }));
-model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+    const xShape = [4, 2];
+    const xData = [[0, 0], [0, 1], [1, 0], [1, 1]];
+    const yShape = [4, 1];
+    const yData = [[0], [0], [0], [1]]; 
+    const epochs = 500;
 
-model.compile({ optimizer: 'sgd', loss: 'binaryCrossentropy', lr: 0.1 });
-// Creating dataset
-const xShape = [4, 2];
-const xData = [[0, 0], [0, 1], [1, 0], [1, 1]];
-const xTensor = tf.tensor2d(xData, xShape);
-//xTensor.print();
+    let xTensor;
+    let yTensor;
+    let model;
 
-const yShape = [4, 1];
-const yData = [[0], [1], [1], [1]];
-const yTensor = tf.tensor2d(yData, yShape);
-//yTensor.print();
-
-// Train the model
-model.fit(xTensor, yTensor, {
-	batchSize: 1,
-	epochs: 500
-}).then((result) => { 
-    const xInput = tf.tensor2d(xData, xShape);
-
-    // Test the model and display output 
-    const predict = model.predict(xInput).dataSync();
+    const main = async () => {
+        createModel();
+        const trainingResult = await trainModel();
+        const predict = await predictModel();
     
-    document.getElementById("output").innerText = predict;
-    console.log(predict);
-    console.log(result.history.loss[999])
-});
+        document.getElementById("output").innerText = predict.map((p) => Math.round(p));
+        console.log(predict.map((p) => Math.round(p)));
+        console.log(predict);
+        console.log(`loss: ${trainingResult.history.loss[epochs -1]}`);
+    }
 
+    const createModel = () => {
+        model = tf.sequential();
+    
+        model.add(tf.layers.dense({ units: 4, inputShape: 2, activation: 'tanh' }));
+        model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+        
+        const learningRate = 0.1;
+        const optimizer = tf.train.sgd(learningRate);
+        
+        model.compile({ optimizer: optimizer, loss: 'binaryCrossentropy', lr: learningRate });
+        // Creating dataset
+        xTensor = tf.tensor2d(xData, xShape);
+        xTensor.print();
+        
+        yTensor = tf.tensor2d(yData, yShape);
+        yTensor.print();
+    }
+    
+    const trainModel = async () => {
+        // Train the model
+        return await model.fit(xTensor, yTensor, {
+            batchSize: 1,
+            epochs: epochs
+        });
+    }
+
+    const predictModel = async () => {
+        const xInput = tf.tensor2d(xData, xShape);
+        
+        // Test the model and display output 
+        const predict = await model.predict(xInput).data();
+        
+        return predict;
+    }
+
+    main();
+});
